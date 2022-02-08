2e4d3ffd45eae3c76440cb5f590a48dfb3212ce6
atulrana007 <atul.rana@zopsmart.com>
2022-02-08 13:15:29 +0530

    updated get token

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,13 +2,29 @@ import React, { Fragment } from "react";
 
 import Hero from "../components/Hero";
 import Content from "../components/Content";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const Home = () => (
-  <Fragment>
-    <Hero />
-    <hr />
-    <Content />
-  </Fragment>
-);
+const Home = () => {
+  const { getAccessTokenSilently } = useAuth0();
+  const GetSomething = async () => {
+    try {
+      const token = await getAccessTokenSilently({
+        audience: "https://api.mcafee.com",
+      });
+      console.log(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Fragment>
+      <Hero />
+      <button onClick={GetSomething}>Click Here</button>
+      <hr />
+      <Content />
+    </Fragment>
+  );
+};
 
 export default Home;
